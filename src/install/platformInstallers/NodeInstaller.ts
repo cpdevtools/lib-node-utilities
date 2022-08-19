4347e4c9d13068bb4948475f78ac2547c197199a
Dan Erdelt <erd@conceptual.ca>
2022-08-19 00:20:55 +0000

    updated node installer

--- src/install/platformInstallers/NodeInstaller.ts
+++ src/install/platformInstallers/NodeInstaller.ts
@@ -1,18 +1,23 @@
+import { exec, run } from "../../utils";
 import { PlatformInstallerBase } from "./PlatformInstallerBase";
 
 export class NodeInstaller extends PlatformInstallerBase {
   public get isInstalled(): Promise<boolean> {
     return (async () => {
-      return true;
+      try {
+        const result = await run(`npm ls -gp ${this.id}`);
+        return !!result.trim();
+      } catch {}
+      return false;
     })();
   }
   public async installOrUpdate(): Promise<void> {
-    console.log("NodeInstaller.installOrUpdate");
+    await exec(`npm install -g ${this.id}@latest`);
   }
   public async update(): Promise<void> {
-    console.log("NodeInstaller.update");
+    await exec(`npm install -g ${this.id}@latest`);
   }
   public async uninstall(): Promise<void> {
-    console.log("NodeInstaller.uninstall");
+    await exec(`npm uninstall -g ${this.id}`);
   }
 }
