a68cff810c4480334ce4762f323db637a67c7f55
Erd <myrddraall@users.noreply.github.com>
2023-09-11 13:19:48 -0400

    templates

---
+++ src/templates/render.ts
@@ -0,0 +1,35 @@
+import { readFile, writeFile } from "fs/promises";
+import { render } from "mustache";
+import { PackageManager } from "../package-managers/PackageManager";
+
+export function renderTemplate(template: string, data: any): string {
+  return render(template, data, undefined);
+}
+
+export async function renderTemplateFile(templatePath: string, data: any): Promise<string> {
+  const tpl = await readFile(templatePath, "utf-8");
+  return renderTemplate(tpl, data);
+}
+
+export async function renderTemplateToFile(toFile: string, template: string, data: any): Promise<void> {
+  await writeFile(toFile, renderTemplate(template, data), "utf-8");
+}
+
+export async function renderTemplateFileToFile(toFile: string, templatePath: string, data: any): Promise<void> {
+  await writeFile(toFile, await renderTemplateFile(templatePath, data), "utf-8");
+}
+
+export async function renderTemplateFileForEachProject(templatePath: string, data: any) {
+  const pm = await PackageManager.loadPackage(process.cwd() + "/package.json");
+  const r = await pm.workspaceCall(
+    async (pkg) => {
+      const result = await renderTemplateFile(templatePath, data);
+      return {
+        projectName: pkg.name,
+        result,
+      };
+    },
+    { parallel: true }
+  );
+  return r.results;
+}
