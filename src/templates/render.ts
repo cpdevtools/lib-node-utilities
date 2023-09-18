96bde242c292916bf252fb156efa110eae108651
Erd <myrddraall@users.noreply.github.com>
2023-09-18 15:35:13 -0400

    ...

--- src/templates/render.ts
+++ src/templates/render.ts
@@ -1,20 +1,31 @@
 import { readFile, writeFile } from "fs/promises";
-import { render } from "mustache";
-import { PackageManager } from "../package-managers/PackageManager";
+import { OpeningAndClosingTags, render } from "mustache";
 
-export function renderTemplate(template: string, data: any): string {
-  return render(template, data, undefined);
+export function renderTemplate(template: string, data: any, tags: OpeningAndClosingTags = ["{{", "}}"]): string {
+  return render(template, data, undefined, {
+    tags,
+  });
 }
 
-export async function renderTemplateFile(templatePath: string, data: any): Promise<string> {
+export async function renderTemplateFile(templatePath: string, data: any, tags: OpeningAndClosingTags = ["{{", "}}"]): Promise<string> {
   const tpl = await readFile(templatePath, "utf-8");
-  return renderTemplate(tpl, data);
+  return renderTemplate(tpl, data, tags);
 }
 
-export async function renderTemplateToFile(toFile: string, template: string, data: any): Promise<void> {
-  await writeFile(toFile, renderTemplate(template, data), "utf-8");
+export async function renderTemplateToFile(
+  toFile: string,
+  template: string,
+  data: any,
+  tags: OpeningAndClosingTags = ["{{", "}}"]
+): Promise<void> {
+  await writeFile(toFile, renderTemplate(template, data, tags), "utf-8");
 }
 
-export async function renderTemplateFileToFile(toFile: string, templatePath: string, data: any): Promise<void> {
-  await writeFile(toFile, await renderTemplateFile(templatePath, data), "utf-8");
+export async function renderTemplateFileToFile(
+  toFile: string,
+  templatePath: string,
+  data: any,
+  tags: OpeningAndClosingTags = ["{{", "}}"]
+): Promise<void> {
+  await writeFile(toFile, await renderTemplateFile(templatePath, data, tags), "utf-8");
 }
