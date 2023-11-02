41ab06e735cb5909c1cfae0ab4cfedc0b240bcb3
Erd <erd@conceptual.ca>
2023-11-02 14:37:44 +0000

    cleanup

--- src/templates/github-workflows.ts
+++ src/templates/github-workflows.ts
@@ -4,10 +4,9 @@ import { PackageManager } from "../package-managers";
 import { renderTemplateFileToFile } from "./render";
 
 export async function renderGithubWorkflowTemplates() {
-  console.log("Rendering GitHub workflow templates...");
+  console.info("Rendering GitHub workflow templates...");
   const pm = await PackageManager.loadPackage("package.json");
   const pkgs = await pm.listWorkspacePackages();
-  console.log(pkgs);
 
   const tplFiles = await glob("*.*.mustache", { onlyFiles: true, dot: true, cwd: "./.github/workflow-templates" });
   tplFiles.forEach(async (tplFile) => {
