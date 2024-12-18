fb49fdd42260ac3ae80c8523eaddae3769802617
Erd <undefined>
2024-12-18 18:00:45 +0000

    chore: bump package version to 0.5.0-dev.6 and integrate jsonc for JSON parsing and stringifying

--- src/utils/json.ts
+++ src/utils/json.ts
@@ -1,9 +1,10 @@
 import { readFile, writeFile } from "fs/promises";
+import { jsonc } from "jsonc";
 
 export async function readJsonFile<T = unknown>(path: string): Promise<T> {
-  return JSON.parse(await readFile(path, { encoding: "utf-8" }));
+  return jsonc.parse(await readFile(path, { encoding: "utf-8" }));
 }
 
 export async function writeJsonFile(path: string, data: any, indent?: number) {
-  await writeFile(path, JSON.stringify(data, undefined, indent));
+  await writeFile(path, jsonc.stringify(data, { space: indent, handleCircular: true }));
 }
